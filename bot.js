84e2dd0e3dd9aae516726c9607b1dcf206bc6440
REXSR <41483292+REXSR@users.noreply.github.com>
2018-07-27 23:44:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,6 +70,30 @@ m.sendMessage(args)
 
 
 
+client.on('message', message => {
+     var prefix ="+"; // هنا تقدر تغيير برفكس البوت
+ if(message.content.startsWith(prefix +"server")){
+if(!message.channel.guild) return message.reply(' ');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("🆔 Server ID:", message.guild.id,true)
+.addField("📅 Created On", message.guild.createdAt.toLocaleString(),true)
+.addField("👑 Owned by",${message.guild.owner.user.username}#${message.guild.owner.user.discriminator})
+.addField("👥 Members ",[${message.guild.memberCount}],true)
+.addField('💬 Channels ',**${message.guild.channels.filter(m => m.type === 'text').size}** + ' text | Voice  '+ **${message.guild.channels.filter(m => m.type === 'voice').size}**,true)
+.addField("🌍 Others " , message.guild.region,true)
+.addField(" 🔐 Roles ",**[${message.guild.roles.size}]** Role,true)
+.setColor('#000000')
+message.channel.sendEmbed(embed)
+
+}
+});
 
 
 
@@ -77,52 +101,6 @@ m.sendMessage(args)
  
 
  
-
- 
-
- 
-
- 
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => { 
-           if (message.content.startsWith(prefix + "id")) {
-     var args = message.content.split(" ").slice(1);
-     let user = message.mentions.users.first();
-     var men = message.mentions.users.first();
-        var heg;
-        if(men) {
-            heg = men
-        } else {
-            heg = message.author
-        }
-      var mentionned = message.mentions.members.first();
-         var h;
-        if(mentionned) {
-            h = mentionned
-        } else {
-            h = message.member
-        }
-               moment.locale('ar-TN');
-    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
-    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
-    .setFooter(`SRB Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
-    .setThumbnail(heg.avatarURL);
-    message.channel.send(id)
-}       });
 
 
 
