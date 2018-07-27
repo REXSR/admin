96bd6ce6f01c0f08255b8bad156564682a7a8903
REXSR <41483292+REXSR@users.noreply.github.com>
2018-07-27 23:58:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,30 +70,6 @@ m.sendMessage(args)
 
 
 
-client.on('message', message => {
-     var prefix ="+"; // هنا تقدر تغيير برفكس البوت
- if(message.content.startsWith(prefix +"server")){
-if(!message.channel.guild) return message.reply(' ');
-const millis = new Date().getTime() - message.guild.createdAt.getTime();
-const now = new Date();
-dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-var embed  = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-.addField("Server ID:", message.guild.id,true)
-.addField("Created On", message.guild.createdAt.toLocaleString(),true)
-.addField("Members ",[${message.guild.memberCount}],true)
-.addField('Channels ',**${message.guild.channels.filter(m => m.type === 'text').size}** + ' text | Voice  '+ **${message.guild.channels.filter(m => m.type === 'voice').size}**,true)
-.addField("Others " , message.guild.region,true)
-.addField("Roles ",**[${message.guild.roles.size}]** Role,true)
-.setColor('#000000')
-message.channel.sendEmbed(embed)
-
-}
-});
-
 
 
 
