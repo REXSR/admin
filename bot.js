b1e6678a77aad4e48fa01dcf62877a7a7a1c8f74
REXSR <41483292+REXSR@users.noreply.github.com>
2018-07-29 15:49:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -277,7 +277,19 @@ client.on("message", message => {
 });
 
 
+client.on("message", message => {
 
+            if (message.content.startsWith(prefix + "bc4")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
                 
 
