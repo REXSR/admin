b61ab79e0a33fe99dfeaf16eabd88958441b1e1e
REXSR <41483292+REXSR@users.noreply.github.com>
2018-07-31 13:42:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ client.on('message', msg => {
 client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('REX'));
+if (message.content.startsWith('Rexsrb'));
  if (message.author.id !== '283355378811666435') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
