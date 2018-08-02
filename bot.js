a26416bfeaae31fc8fff49a2aa2172857f18e5d4
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-02 20:30:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = '+'
 client.on('ready', () => {
  console.log('Logged in as ${client.user.tag}!');
-client.user.setActivity('By:REX SRB BEST YOO',{ type: 'WATCHING'}) 
+client.user.setActivity('NonyBot',{ type: 'streaming'}) 
 });
 
 
