eff0993627d2cb2a90803bc024010da862e259da
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-08 13:33:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,6 +118,14 @@ client.on('message', message => {
 });
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
 
 
 
