47bdf56166c12ce771df75430d442c24b8fa11d6
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-08 08:24:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,10 +12,28 @@ client.on('ready', () => {
 
 });
 
-
-
-
-
+client.on('message', message => {
+  if (true) {
+if (message.content === '.invite') {
+      message.author.send('   https://discordapp.com/oauth2/authorize?client_id=463837201092968448&scope=bot&permissions=1  |  تفضل ربط البوت     ').catch(e => console.log(e.stack));
+ 
+    }
+   }
+  });
+ 
+ 
+client.on('message', message => {
+     if (message.content === ".invite") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+     
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 
