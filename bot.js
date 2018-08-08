5a1e1f9b0ce60d4dbb4fe6058d534e3829fbc502
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-08 08:13:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,7 @@ message.author.sendEmbed(embed)
 
 
        client.on('message', message => {
-if(message.content === prefix + inv'); {
+if(message.content === prefix + inv');
 let embed = new Discord.RichEmbed ()
 embed.setTitle("** Click To Invite This BOT **")
 .setURL("https://discordapp.com/oauth2/authorize?client_id=463837201092968448&scope=bot&permissions=1");
