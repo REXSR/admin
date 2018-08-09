8a6d8ba680fe2ee8b396ccaf7ce1dd9c77796519
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-10 01:04:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -373,6 +373,26 @@ client.on('guildMemberAdd', member => {
     
 
 
+client.on("message", message => {
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('? | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+}); 
+
+
+
+
 
 
 
