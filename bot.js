5f64cd52ff213daf8a19544b537d4596f93cfe52
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-10 00:54:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -352,6 +352,27 @@ client.on('message',message =>{
 
 
 
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'wlc');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+     .setFooter("**SERVER NAME**")
+        .setTimestamp()
+      channel.sendEmbed(embed);
+    });
+
+    
+
+
 
 
 
