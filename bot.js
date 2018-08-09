71498280caede487cef34838304004f4b3f28751
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-10 01:10:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -394,6 +394,20 @@ client.on("message", message => {
 
 
 
+client.on('message', message => {
+       if (message.content.startsWith(prefix + 'botserver')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#ebf442");
+  message.channel.send(embed);
+}
+});
+
+
+
+
 
 
 
