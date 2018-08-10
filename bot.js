f4d51a52c1c1ea635d819d7397c708d005d59a17
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-10 21:12:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,22 +15,14 @@ client.on('ready', () => {
 client.on('message', message => {
   if (true) {
 if (message.content === 'inv') {
-      message.author.send('   https://discordapp.com/oauth2/authorize?client_id=463837201092968448&scope=bot&permissions=1  |  تفضل ربط البوت     ').catch(e => console.log(e.stack));
-
+      message.author.send('https://discordapp.com/oauth2/authorize?client_id=463837201092968448&scope=bot&permissions=1|تفضل ربط البوت').catch(e => console.log(e.stack));
     }
    }
   });
  
  
-client.on('message', message => {
-     if (message.content === "inv") {
-     let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#9B59B6")
-  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
-  message.channel.sendEmbed(embed);
-    }
-});
+    
+
 
 
 
@@ -125,7 +117,46 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 
-
+client.on('message', message => {
+        var prefix = '!'; // هنا تقدر تغير البرفكس
+	var command = message.content.split(" ")[0];
+	if(command == prefix + 'bc') { // الكوماند !bc
+		var args = message.content.split(' ').slice(1).join(' ');
+		if(message.author.bot) return;
+		if(!args) return message.channel.send(`**➥ Useage:** ${prefix}bc كلامك`);
+		
+		let bcSure = new Discord.RichEmbed()
+		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
+		.setThumbnail(client.user.avatarURL)
+		.setColor('RANDOM')
+		.setDescription(`**\n:envelope: ➥ رسالتك**\n\n${args}`)
+		.setTimestamp()
+		.setFooter(message.author.tag, message.author.avatarURL)
+		
+		message.channel.send(bcSure).then(msg => {
+			msg.react('✅').then(() => msg.react('❎'));
+			message.delete();			
+			let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+			let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+			let sendBC = msg.createReactionCollector(yesEmoji);
+			let dontSendBC = msg.createReactionCollector(noEmoji);
+			sendBC.on('collect', r => {
+				message.guild.members.forEach(member => {
+					member.send(args.replace(`[user]`, member)).catch();
+					if(message.attachments.first()){
+						member.sendFile(message.attachments.first().url).catch();
+					}
+				})
+				message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
+				msg.delete();
+			})
+			dontSendBC.on('collect', r => {
+				msg.delete();
+				message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
+			});
+		})
+	}
+});
 
 
 
