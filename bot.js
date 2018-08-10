554705f40498fc3424e12ce6e9e4c878f85112d4
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-10 06:15:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -429,7 +429,25 @@ Server Count: __${guild.memberCount}__**`)
 
 
 
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '471972617310699530').setName("W");
+client.channels.find('id', '471972617310699530').setName("We");
+client.channels.find('id', '471972617310699530').setName("Wel");
+client.channels.find('id', '471972617310699530').setName("Welc");
+client.channels.find('id', '471972617310699530').setName("Welco");
+client.channels.find('id', '471972617310699530').setName("Welcom");
+client.channels.find('id', '471972617310699530').setName("Welcome");
+client.channels.find('id', '471972617310699530').setName("Welcome T");
+client.channels.find('id', '471972617310699530').setName("Welcome To");
+client.channels.find('id', '471972617310699530').setName("Welcome To S");
+client.channels.find('id', '471972617310699530').setName("Welcome To SR");
+client.channels.find('id', '471972617310699530').setName("Welcome To SRB");
+client.channels.find('id', '471972617310699530').setName("Welcome To SRB");
+  }, 3000);
+});
 
 
 
