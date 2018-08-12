e87fb95c90e02a642aa1e544e29074e22fd3e8c2
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-12 16:39:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -638,6 +638,49 @@ client.on('message', message => {
 });
 
 
+const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
+const coolDown = new Set();
+client.on('message',async message => {
+if(message.author.bot) return;
+if(!credits[message.author.id]) credits[message.author.id] = {
+    credits: 50
+};
+
+let userData = credits[message.author.id];
+let m = userData.credits;lp
+fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
+    if (err) console.error(err);
+  });
+  credits[message.author.id] = {
+      credits: m + 0.5,
+  }
+    if(message.content.startsWith(prefix + "%credit" || prefix + "credits")) {
+message.channel.send(**${message.author.username}, your 💳 balance is \`${userData.credits}``.);
+}
+});
+client.on('message', async message => {
+    let amount = 250;
+    if(message.content.startsWith(prefix + "%daily")) {
+    if(message.author.bot) return;
+    if(coolDown.has(message.author.id)) return message.channel.send(⏱ | ${message.author.username}, your daily 💴 credits refreshes in ``1 Day``.);
+    let userData = credits[message.author.id];
+    let m = userData.credits + amount;
+    credits[message.author.id] = {
+    credits: m
+    };
+    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
+    if (err) console.error(err);
+    });
+    message.channel.send(🏧 | ${message.author.username}, you received your 💴 ${amount} credits!**`).then(() => {
+        coolDown.add(message.author.id);
+    });
+    setTimeout(() => {
+       coolDown.remove(message.author.id);
+    },86400000);
+    }
+});
+
+
 
 
 
