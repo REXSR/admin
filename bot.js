4de754f0d7093249508694ab54c87fc48ab82969
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-12 07:20:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,6 +77,20 @@ message.author.sendEmbed(embed)
 }
 }); 
 
+client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
 
 
 client.on('message', message => {
