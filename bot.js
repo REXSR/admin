63fe1e1a6bfc6875f11c36df2616dd8becf12bbf
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-15 21:03:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -417,7 +417,7 @@ client.on("guildMemberAdd", member => {
     let memberavatar = member.user.avatarURL
       if (!welcomer) return;
       if(welcomer) {
-         moment.locale('ar-ly');
+         moment locale('ar-ly');
          var h = member.user;
         let heroo = new Discord.RichEmbed()
         .setColor('RANDOM')
