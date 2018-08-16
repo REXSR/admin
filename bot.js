6cda19ed8f7f6b9d48de0b31922651bf838b6e45
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-17 00:33:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,23 +83,8 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.on('message',async message => {
-  if(message.content.startsWith(prefix + "setvoice")) {
-  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply(' :x: ليس لديك الصلاحيات الكافية');
-  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('x ليس معي الصلاحيات الكافية');
-  message.channel.send('white_check_mark| تم عمل الروم بنجاح');
-  message.guild.createChannel(Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ] , 'voice').then(c => {
-    console.log(Voice online channel setup for guild: \n ${message.guild.name});
-    c.overwritePermissions(message.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-    setInterval(() => {
-      c.setName(Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ])
-    },1000);
-  });
-  }
-}); 
+
+ 
       
 
 	
