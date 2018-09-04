87d9008466e6bdd5a9501862ba535cf48997732d
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-05 02:42:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -334,7 +334,71 @@ var mentionned = message.mentions.members.first();
   
      
     
+const fs = require("fs");
+  client.on('message', async message =>{
+      let messageArray = message.content.split(" ");
+      let cmd = messageArray[0];
+      let args = messageArray.slice(1);
+      let xp = require("./xp.json");
+  
+    let xpAdd = Math.floor(Math.random() * 7) + 8;
+    console.log(xpAdd);
+  
+    if(!xp[message.author.id]){
+      xp[message.author.id] = {
+        xp: 0,
+        level: 1
+      };
+    }
+  
+  
+    let curxp = xp[message.author.id].xp;
+    let curlvl = xp[message.author.id].level;
+    let nxtLvl = xp[message.author.id].level * 300;
+    xp[message.author.id].xp =  curxp + xpAdd;
+    if(nxtLvl <= xp[message.author.id].xp){
+      xp[message.author.id].level = curlvl + 1;
+      let lvlup = new Discord.RichEmbed()
+      .setTitle("Level Up!")
+      .setColor("blue")
+      .addField("New Level", curlvl + 1);
+  
+      message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+    }
+    fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+      if(err) console.log(err)
+    });
+  });
 
+  client.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(0);
+    let prefix = '.';
+    let xp = require("./xp.json");
+    
+if(cmd === `${prefix}level`) {
+if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+   };
+ }
+   let curxp = xp[message.author.id].xp;
+   let curlvl = xp[message.author.id].level;
+   let nxtLvlXp = curlvl * 300;
+   let difference = nxtLvlXp - curxp;
+ 
+   let lvlEmbed = new Discord.RichEmbed()
+   .setAuthor(message.author.username)
+   .setColor("blue")
+   .addField("Level", curlvl, true)
+   .addField("XP", curxp, true)
+   .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
+ 
+   message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
+}
+});
          
 
   
