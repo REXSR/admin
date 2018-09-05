0d52dc75e8cf5330caf5426f972414c4c427befc
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-05 05:37:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -380,7 +380,6 @@ if(!xp[message.author.id]){
 
  
 
-const fs = require("fs");
 const moment = require('moment');
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
