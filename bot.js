af0728121d8b2a15956edc3ee53474d0d41ea0ab
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-05 05:23:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.user.setGame(`Great.`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-
+ 
  
 
 
@@ -354,7 +354,7 @@ if(cmd === `${prefix}level`) {
 if(!xp[message.author.id]){
     xp[message.author.id] = {
       xp: 0,
-      level: 1
+      level: 0,
    };
  }
    let curxp = xp[message.author.id].xp;
@@ -385,7 +385,7 @@ client.on("message", (message) => {
   if (message.author.bot) return;
     if (message.author.id === client.user.id) return;
 	if(!message.channel.guild) return;       
-if (message.content === 'credits') {
+if (message.content === '.credits') {
 message.channel.send(`** ${message.author.username}, your :credit_card: balance is ${games[message.author.id].credits}.**`)
 }
 });
@@ -393,31 +393,7 @@ message.channel.send(`** ${message.author.username}, your :credit_card: balance 
 
 
 
-#User "!userinfo [username or blank]": displays info for the selected user or for message sender if arguments are not provided
-
-#Music "!play [song name or blank]": plays a song or if arguments are not provided plays the first song in the queue
-
-".skip": skips the current song
-
-".queue": displays current queue
-
-".queue remove [song number]": removes chosen song from the queue
-
-".queue clear": removes all songs from the queue
-
-".queue shuffle": shuffles the current queue
-
-".repeat": plays current song again
-
-".stop": stops playing music and deletes all songs in the queue
-
-".yt [search term]": searches the YouTube and returns first 5 results
-
-".add [search result number]": adds a song from YouTube search to the queue
-
-".vol [percentage]": sets the volume of the music to given percentage
-
-#Misc "!help or !commands": displays avialable bot commands
+  
  
 
 
