9423cd1ba94f544f93d863b4390c9f3525046271
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-05 05:18:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,7 +393,32 @@ message.channel.send(`** ${message.author.username}, your :credit_card: balance 
 
 
 
+#User "!userinfo [username or blank]": displays info for the selected user or for message sender if arguments are not provided
 
+#Music "!play [song name or blank]": plays a song or if arguments are not provided plays the first song in the queue
+
+".skip": skips the current song
+
+".queue": displays current queue
+
+".queue remove [song number]": removes chosen song from the queue
+
+".queue clear": removes all songs from the queue
+
+".queue shuffle": shuffles the current queue
+
+".repeat": plays current song again
+
+".stop": stops playing music and deletes all songs in the queue
+
+".yt [search term]": searches the YouTube and returns first 5 results
+
+".add [search result number]": adds a song from YouTube search to the queue
+
+".vol [percentage]": sets the volume of the music to given percentage
+
+#Misc "!help or !commands": displays avialable bot commands
+ 
 
 
   
