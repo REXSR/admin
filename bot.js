dc4b37f607770736a162d24618478dd60aff6cb4
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-08 17:51:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -532,20 +532,48 @@ client.on('message', async message => {
 
 
 
-client.on('message', message => {
-if (message.content === 'هاي سبام') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+client.on("message", message => {
+ 
+  function discoRole() {
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    roles.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      theRole.edit({color: random}).catch(e => {
+        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
+      });
+    });
+  }
+ 
+  if(message.content.startsWith(prefix + "startdisco")) {
+    if(allowedUsers.includes(message.author.id)) {
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.send("```css\nDiscoing...```");
+    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
+  } else {
+    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+} else
+ 
+if(message.content.startsWith(prefix + "stopdisco")) {
+  if(allowedUsers.includes(message.author.id)) {
+  message.channel.send("I've stopped discoing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+}
+ 
 });
-
+const config = require('config.json');
+const prefix = config.prefix;
+ 
+const allowedUsers = config.allowedUsers;
+const roles = config.roleToDisco;
+ 
+client.on("ready", () => {
+    client.user.setPresence({ game: { name: `Disco Roles! Created by i am toast#1213` }, type: 0 });
+    console.log("Disco role bot online! Created by i am toast.");
+});
   
  
 
