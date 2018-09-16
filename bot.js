bab7b73216cbe20f7ce73e041a368248fda404d8
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-16 22:40:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -149,6 +149,51 @@ var mentionned = message.mentions.members.first();
          
      });
  
+client.on('message', message => {
+          
+
+           if (message.content.startsWith(prefix + "id")) {
+           
+if (message.channel.id !== "326101086467522570") return;
+if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات ❌`);
+
+                message.guild.fetchInvites().then(invs => {
+      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
+      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+      var moment = require('moment');
+      var args = message.content.split(" ").slice(1);
+let user = message.mentions.users.first();
+var men = message.mentions.users.first();
+ var heg;
+ if(men) {
+     heg = men
+ } else {
+     heg = message.author
+ }
+var mentionned = message.mentions.members.first();
+  var h;
+ if(mentionned) {
+     h = mentionned
+ } else {
+     h = message.member
+ }
+        moment.locale('ar-TN');
+      var id = new  Discord.RichEmbed()
+       
+    .setColor("#0a0909")
+    .setAuthor(message.author.username, message.author.avatarURL) 
+.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
+.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
+.addField(` :لقد قمت بدعوة `, ` ${inviteCount} `)
+.setFooter("BOT")  
+    message.channel.sendEmbed(id);
+})
+}
+    
+
+         
+     });
 
 
 
